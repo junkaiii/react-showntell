deaac3ec109ec9ccf43be123b0337b80e2c22fb0
Jun Kai Lim <junkaiii@gmail.com>
2016-09-06 18:36:35 +0800

    hello world done version

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class HelloWorld extends React.Component {
+  render() {
+    return(
+      <div>Hello World</div>
+    )
+  }
+}
+
+
+
+render(<HelloWorld />, document.getElementById('app'))
